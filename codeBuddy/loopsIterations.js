6e849e10afb4ec1ceff79116135f1180e35f1933
Itinayan <itinayansenapati@gmail.com>
2022-04-03 17:50:24 +0530

    Solutions

--- codeBuddy/loopsIterations.js
+++ codeBuddy/loopsIterations.js
@@ -1,25 +1,22 @@
 // level {1/2/3}
 //LoopsIterations Lvel-2
 
-const calculateSum = (input) => {
-  let fibo = [];
-  let sum=[];
-  if (n <= 0) return 0;
-
-  fibo[0] = 0;
-  fibo[1] = 1;
-
-  // Initialize result
-  let total = fibo[0] + fibo[1];
-
-  // Add remaining terms
+const fibonancciSries = (input) => {
+  let firstNum = 0,
+    secondNum = 1,
+    total = 1,
+    nextNum;
+  document.writeln("Fibonance Sries :");
+  document.writeln(`${secondNum},`);
   for (let i = 2; i <= input; i++) {
-    fibo[i] = fibo[i - 1] + fibo[i - 2];
-    sum.push(fibo[i]);
-    total += fibo[i];
+    nextNum = firstNum + secondNum;
+    document.writeln(`${nextNum},`);
+    total = total + firstNum + secondNum;
+    firstNum = secondNum;
+    secondNum = nextNum;
   }
-
-  return {sum, total};
+  document.write(`<br />`);
+  document.write(`Total: ${total}`);
 };
-calculateSum(8).sum.forEach(element=>console.log(element))
-console.log("Sum :",calculateSum(8).total)
+fibonancciSries(6);
+
